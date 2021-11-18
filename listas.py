315eb942e685ba312ab3ae46d744eb2e6711c583
alexpalma98 <83353323+alexpalma98@users.noreply.github.com>
2021-11-18 06:40:14 -0500

    Update listas.py

--- listas.py
+++ listas.py
@@ -1,1 +1,7 @@
+#EJERCICIO DEL CURSO FUNDAMENTOS DE PROGRAMACIÓN
 
+texto = ''' por que es hermoso ser joven? ¿Por que el sueño de la juventud perenne? Me parece que son dos los elementos determinantes. La juventud tiene todavia el futuro por delante; todo es futuro, tiempo de esperanza. El futuro esta lleno de promesas.Para ser sinceros, debemos decir que para muchos el futuro también se presenta oscuro, sembrado de amenazas. Hay incertidumbre: ¿encontrare un puesto de trabajo?, ¿encontrare una vivienda?, ¿encontrare el amor?, ¿cual sera mi verdadero futuro? Y ante estas amenazas, el futuro tambien puede presentarse como un gran vacio. Por eso, hoy muchos quieren detener el tiempo, por miedo a un futuro en el vacio. Quieren aprovechar al maximo inmediatamente todas las bellezas de la vida. Y asi el aceite en la lampara se consuma cuando la vida deberia comenzar. Por eso es importante elegir las verdaderas promesas, que abren al futuro, incluso con renuncias'''
+
+#La siguiente función retorna una lista de las palabras del mensaje y otras cuyos valores son los número de veces
+#de cada palabra se repite en el texto.
+#Convierte todas las palabras a minúsculas (Ignore la diferencia entre mayúsculas y minúsculas)
